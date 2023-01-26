f46e0a122046141ed0e4352e3afa2337a115796f
GaZaTu <gazatu@protonmail.com>
2023-01-26 21:14:27 +0100

    rename targets and shit

--- .scripts/fcitx5-install.sh
+++ .scripts/fcitx5-install.sh
@@ -9,7 +9,7 @@ mkdir -m 755 -p /usr/share/fcitx5/inputmethod
 ln -s $(realpath $__DIRNAME/../fcitx5-inputmethod.conf) /usr/share/fcitx5/inputmethod/fcitx5imemojipicker.conf
 
 mkdir -m 755 -p /usr/lib/fcitx5
-ln -s $(realpath $__DIRNAME/../build/im-emoji-picker-fcitx5.so) /usr/lib/fcitx5/fcitx5imemojipicker.so
+ln -s $(realpath $__DIRNAME/../build/fcitx5imemojipicker.so) /usr/lib/fcitx5/fcitx5imemojipicker.so
 
 mkdir -m 755 -p /usr/share/icons/hicolor/32x32/apps
 ln -s $(realpath $__DIRNAME/../src/res/im-emoji-picker_32x32.png) /usr/share/icons/hicolor/32x32/apps/im-emoji-picker.png
