97129c44d506ea782277651391077e40747c85b4
GaZaTu <gazatu@protonmail.com>
2023-02-23 20:03:10 +0100

    implement as fcitx5 module instead of engine

--- .scripts/fcitx5-install.sh
+++ .scripts/fcitx5-install.sh
@@ -5,9 +5,6 @@ __DIRNAME=$(realpath $(dirname "$0"))
 mkdir -m 755 -p /usr/share/fcitx5/addon
 ln -s $(realpath $__DIRNAME/../fcitx5-addon.conf) /usr/share/fcitx5/addon/fcitx5imemojipicker.conf
 
-mkdir -m 755 -p /usr/share/fcitx5/inputmethod
-ln -s $(realpath $__DIRNAME/../fcitx5-inputmethod.conf) /usr/share/fcitx5/inputmethod/fcitx5imemojipicker.conf
-
 mkdir -m 755 -p /usr/lib/fcitx5
 ln -s $(realpath $__DIRNAME/../build/fcitx5imemojipicker.so) /usr/lib/fcitx5/fcitx5imemojipicker.so
 
---
+++ src/Fcitx5ImEmojiPickerModule.cpp
@@ -0,0 +1,238 @@
+#if __has_include("fcitx/addoninstance.h")
+
+#include "Fcitx5ImEmojiPickerModule.hpp"
+#include "EmojiPickerWindow.hpp"
+#include "logging.hpp"
+#include <QKeyEvent>
+#include <QProcess>
+#include <fcitx-config/configuration.h>
+#include <fcitx-config/enum.h>
+#include <fcitx-config/iniparser.h>
+#include <fcitx-utils/key.h>
+#include <fcitx-utils/keysym.h>
+#include <fcitx-utils/keysymgen.h>
+#include <fcitx/event.h>
+#include <fcitx/inputcontext.h>
+#include <thread>
+
+#define KEYCODE_ESCAPE 9
+#define KEYCODE_RETURN 36
+#define KEYCODE_BACKSPACE 22
+#define KEYCODE_TAB 23
+#define KEYCODE_ARROW_UP 111
+#define KEYCODE_ARROW_DOWN 116
+#define KEYCODE_ARROW_LEFT 113
+#define KEYCODE_ARROW_RIGHT 114
+#define KEYCODE_PAGE_UP 112
+#define KEYCODE_PAGE_DOWN 117
+#define KEYCODE_F4 70
+
+#define KEYCODE_SPACE 65
+#define KEYCODE_UNDERSCORE 61
+
+Fcitx5ImEmojiPickerModule::Fcitx5ImEmojiPickerModule(fcitx::Instance* instance) : _instance(instance) {
+  resetInputMethodEngine = [this]() {
+    _active = false;
+  };
+
+  _eventHandlers.emplace_back(_instance->watchEvent(fcitx::EventType::InputContextKeyEvent, fcitx::EventWatcherPhase::Default, [this](fcitx::Event& _event) {
+    auto& event = static_cast<fcitx::KeyEvent&>(_event);
+
+    if (event.isRelease()) {
+      return;
+    }
+
+    if (!event.key().checkKeyList(*_config.triggerKey)) {
+      return;
+    }
+
+    event.filterAndAccept();
+    activate(event);
+  }));
+
+  _eventHandlers.emplace_back(_instance->watchEvent(fcitx::EventType::InputContextFocusOut, fcitx::EventWatcherPhase::Default, [this](fcitx::Event& _event) {
+    auto& event = static_cast<fcitx::InputContextEvent&>(_event);
+
+    deactivate(event);
+  }));
+
+  _eventHandlers.emplace_back(_instance->watchEvent(fcitx::EventType::InputContextReset, fcitx::EventWatcherPhase::Default, [this](fcitx::Event& _event) {
+    auto& event = static_cast<fcitx::InputContextEvent&>(_event);
+
+    deactivate(event);
+  }));
+
+  _eventHandlers.emplace_back(_instance->watchEvent(fcitx::EventType::InputContextSwitchInputMethod, fcitx::EventWatcherPhase::Default, [this](fcitx::Event& _event) {
+    auto& event = static_cast<fcitx::InputContextEvent&>(_event);
+
+    deactivate(event);
+  }));
+
+  _eventHandlers.emplace_back(_instance->watchEvent(fcitx::EventType::InputContextKeyEvent, fcitx::EventWatcherPhase::PreInputMethod, [this](fcitx::Event& _event) {
+    auto& event = static_cast<fcitx::KeyEvent&>(_event);
+
+    if (!_active) {
+      return;
+    }
+
+    event.filter();
+    keyEvent(event);
+  }));
+
+  reloadConfig();
+}
+
+void Fcitx5ImEmojiPickerModule::keyEvent(fcitx::KeyEvent& keyEvent) {
+  log_printf("[debug] Fcitx5ImEmojiPickerModule::keyEvent key:%s code:%d isRelease:%d\n", keyEvent.key().toString().data(), keyEvent.key().code(), keyEvent.isRelease());
+
+  if (keyEvent.key().isModifier()) {
+    return;
+  }
+
+  QKeyEvent::Type _type = keyEvent.isRelease() ? QKeyEvent::KeyRelease : QKeyEvent::KeyPress;
+  int _key = 0;
+  switch (keyEvent.key().code()) {
+  case KEYCODE_ESCAPE: // FcitxKey_Escape:
+    _key = Qt::Key_Escape;
+    break;
+  case KEYCODE_RETURN: // FcitxKey_Return:
+    _key = Qt::Key_Return;
+    break;
+  case KEYCODE_BACKSPACE: // FcitxKey_BackSpace:
+    _key = Qt::Key_Backspace;
+    break;
+  case KEYCODE_TAB: // FcitxKey_Tab:
+    _key = Qt::Key_Tab;
+    break;
+  case KEYCODE_ARROW_UP: // FcitxKey_uparrow:
+    _key = Qt::Key_Up;
+    break;
+  case KEYCODE_ARROW_DOWN: // FcitxKey_downarrow:
+    _key = Qt::Key_Down;
+    break;
+  case KEYCODE_ARROW_LEFT: // FcitxKey_leftarrow:
+    _key = Qt::Key_Left;
+    break;
+  case KEYCODE_ARROW_RIGHT: // FcitxKey_rightarrow:
+    _key = Qt::Key_Right;
+    break;
+  case KEYCODE_PAGE_UP: // FcitxKey_pageup:
+    _key = Qt::Key_PageUp;
+    break;
+  case KEYCODE_PAGE_DOWN: // FcitxKey_pagedown:
+    _key = Qt::Key_PageDown;
+    break;
+  case KEYCODE_F4: // FcitxKey_f4:
+    _key = Qt::Key_F4;
+    break;
+  }
+  Qt::KeyboardModifiers _modifiers = Qt::NoModifier;
+  if (keyEvent.key().states() & fcitx::KeyState::Super) {
+    _modifiers |= Qt::MetaModifier;
+  }
+  if (keyEvent.key().states() & fcitx::KeyState::Ctrl) {
+    _modifiers |= Qt::ControlModifier;
+  }
+  if (keyEvent.key().states() & fcitx::KeyState::Shift) {
+    _modifiers |= Qt::ShiftModifier;
+  }
+  QString _text = QString::fromStdString(keyEvent.key().toString());
+  _text = _text.right(1);
+
+  switch (keyEvent.key().code()) {
+  case KEYCODE_SPACE: // FcitxKey_Space:
+    _text = " ";
+    break;
+  case KEYCODE_UNDERSCORE: // FcitxKey_Underscore:
+    _text = "_";
+    break;
+  }
+
+  QKeyEvent* qevent = new QKeyEvent(_type, _key, _modifiers, _text);
+  EmojiAction action = getEmojiActionForQKeyEvent(qevent);
+
+  if (action != EmojiAction::INVALID) {
+    emojiCommandQueue.push(std::make_shared<EmojiCommandProcessKeyEvent>(qevent));
+
+    keyEvent.accept();
+  } else {
+    delete qevent;
+  }
+
+  if (_key == Qt::Key_Return && _type == QKeyEvent::KeyRelease) {
+    sendCursorLocation(keyEvent);
+  }
+}
+
+void Fcitx5ImEmojiPickerModule::activate(fcitx::InputContextEvent& event) {
+  if (_active) {
+    return;
+  }
+
+  log_printf("[debug] Fcitx5ImEmojiPickerModule::activate\n");
+
+  _active = true;
+
+  emojiCommandQueue.push(std::make_shared<EmojiCommandEnable>([this, inputContext{event.inputContext()}](const std::string& text) {
+    inputContext->commitString(text);
+
+    // usleep(10000);
+    // sendCursorLocation(inputContext);
+  }));
+
+  sendCursorLocation(event);
+}
+
+void Fcitx5ImEmojiPickerModule::deactivate(fcitx::InputContextEvent& event) {
+  if (!_active) {
+    return;
+  }
+
+  log_printf("[debug] Fcitx5ImEmojiPickerModule::deactivate\n");
+
+  _active = false;
+
+  emojiCommandQueue.push(std::make_shared<EmojiCommandDisable>());
+}
+
+void Fcitx5ImEmojiPickerModule::reset(fcitx::InputContextEvent& event) {
+  log_printf("[debug] Fcitx5ImEmojiPickerModule::reset\n");
+
+  emojiCommandQueue.push(std::make_shared<EmojiCommandReset>());
+}
+
+void Fcitx5ImEmojiPickerModule::sendCursorLocation(fcitx::InputContextEvent& event) {
+  const fcitx::Rect& r = event.inputContext()->cursorRect();
+
+  log_printf("[debug] Fcitx5ImEmojiPickerModule::sendCursorLocation x:%d y:%d w:%d h:%d\n", r.left(), r.top(), r.width(), r.height());
+
+  emojiCommandQueue.push(std::make_shared<EmojiCommandSetCursorLocation>(new QRect(r.left(), r.top(), r.width(), r.height())));
+}
+
+void Fcitx5ImEmojiPickerModule::reloadConfig() {
+  fcitx::readAsIni(_config, CONFIG_FILE);
+}
+
+const fcitx::Configuration* Fcitx5ImEmojiPickerModule::getConfig() const {
+  return &_config;
+}
+
+void Fcitx5ImEmojiPickerModule::setConfig(const fcitx::RawConfig& config) {
+  _config.load(config, true);
+  safeSaveAsIni(_config, CONFIG_FILE);
+}
+
+fcitx::AddonInstance* Fcitx5ImEmojiPickerModuleFactory::create(fcitx::AddonManager* manager) {
+  log_printf("[debug] Fcitx5ImEmojiPickerModuleFactory::create\n");
+
+  static bool startedGUIThread = false;
+  if (!startedGUIThread) {
+    std::thread{gui_main, 0, nullptr}.detach();
+  }
+
+  return new Fcitx5ImEmojiPickerModule(manager->instance());
+}
+
+FCITX_ADDON_FACTORY(Fcitx5ImEmojiPickerModuleFactory);
+
+#endif
